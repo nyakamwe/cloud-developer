17525861a6887100e8fca52b5fdd829604006c49
nyakamwe <nyakamweaimable@gmail.com>
2022-10-09 17:08:23 +0200

    working on project 04

--- course-04/project/c4-final-project-starter-code/client/src/components/EditTodo.tsx
+++ course-04/project/c4-final-project-starter-code/client/src/components/EditTodo.tsx
@@ -52,13 +52,13 @@ export class EditTodo extends React.PureComponent<
 
       this.setUploadState(UploadState.FetchingPresignedUrl)
       const uploadUrl = await getUploadUrl(this.props.auth.getIdToken(), this.props.match.params.todoId)
-
+      console.log('uploadUrl', uploadUrl)
       this.setUploadState(UploadState.UploadingFile)
       await uploadFile(uploadUrl, this.state.file)
 
       alert('File was uploaded!')
-    } catch (e) {
-      alert('Could not upload a file: ' + (e as Error).message)
+    } catch (e: any) {
+      alert('Could not upload a file: ' + e.message)
     } finally {
       this.setUploadState(UploadState.NoUpload)
     }
--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,11 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = '7flj868twl'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-cl4qytzv.us.auth0.com',            // Auth0 domain
+  clientId: 'a3pD8ohZrbgj7qlSvhaXV8Fb8wItazA5',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
