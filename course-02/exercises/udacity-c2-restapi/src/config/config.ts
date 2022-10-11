bb1db7bb01696ca071f1e3b1de482b77e00f47b3
nyakamwe <nyakamweaimable@gmail.com>
2022-10-11 07:18:43 +0200

    fix issues for loading todo's

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,9 +1,9 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "udagramfilterdev",
+    "password": "udagramfilterdev",
+    "database": "udagramfilterdev",
+    "host": "udagramfilterdev.cuswdykyyjrp.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_region": "us-east-2",
     "aws_profile": "default",
--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '7flj868twl'
+const apiId = '855tt1b135'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 
 export const authConfig = {
